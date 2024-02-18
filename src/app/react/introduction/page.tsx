ec5c274e82c1f0caab4f5ead554407eb9fa12143
arefmaghdam <arefmaghdam@gmail.com>
2024-02-18 13:28:03 +0330

    feat: review of react introduction

---
+++ src/app/react/introduction/page.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from "react";
+import Link from "next/link";
+import CodePreview from "../../../components/code-preview";
+
+const Caching: FC = () => {
+  return (
+    <div>
+      <Link href="/">back</Link>
+      <CodePreview
+        imgURL=""
+        content={`
+    // React => a javascript library for UI development made by facebook
+    // library => a set of functions and classes that we use them to develope apps simplicity
+    // when we use React? => if our app is more complex ans if we need better performance 
+    // better performance => VDOM and reconcilation
+    // management of view in react => based on components
+      `}
+      />
+    </div>
+  );
+};
+
+export default Caching;
